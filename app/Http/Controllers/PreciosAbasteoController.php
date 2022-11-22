942b56e964d9988adbff762a1f68b23e4aa259d2
Edgar Hdez <edgaarhdez96@gmail.com>
2022-11-22 11:58:08 -0600

    Últimos cambios

--- app/Http/Controllers/PreciosAbasteoController.php
+++ app/Http/Controllers/PreciosAbasteoController.php
@@ -97,9 +97,10 @@ class PreciosAbasteoController extends Controller
             $headers = ['Content-Type' => 'application/json'];
             $url = "https://www.abasteo.mx/api/v0.1/catalog/filter?type=search&id=".$sku;
             $res = $client->request('GET', $url, ['headers' => [
-                'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
-                'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-              ]]);
+                'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+                'User-Agent' => "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+            ]]);
             // $res = $client->request('GET', $url);
             $result = $res->getBody();
             $data = json_decode($result, true);

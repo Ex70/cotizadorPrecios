246fd72f11a7ed9cd313c2f760a9ec6d8d340370
Carlo Rdgz <isc_carlo_rodriguez@hotmail.com>
2023-01-26 18:08:41 -0600

    LecturaN4

--- app/Console/Commands/jsonLocal.php
+++ app/Console/Commands/jsonLocal.php
@@ -33,8 +33,8 @@ class jsonLocal extends Command
         //dd($fileExist);
         if ($fileExist) {
             $size = storage_path('catalogo_xml/productos.json');
-            //Storage::disk('local')->put('public/products.json',Storage::disk('prueba-ftp')->get('catalogo_xml/productos.json'));
-            Storage::disk('local')->put('public/products-' . filesize($size) . '.json', Storage::disk('prueba-ftp')->get('catalogo_xml/productos.json'));
+            Storage::disk('local')->put('public/products.json',Storage::disk('prueba-ftp')->get('catalogo_xml/productos.json'));
+            //Storage::disk('local')->put('public/products-' . filesize($size) . '.json', Storage::disk('prueba-ftp')->get('catalogo_xml/productos.json'));
         } else {
             dd("No existe");
         }

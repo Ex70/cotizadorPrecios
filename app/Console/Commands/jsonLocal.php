ca41538b8bde195c220e294a8d746f0de45e4325
Carlo Rdgz <isc_carlo_rodriguez@hotmail.com>
2023-01-27 13:47:25 -0600

    cron2

--- app/Console/Commands/jsonLocal.php
+++ app/Console/Commands/jsonLocal.php
@@ -30,7 +30,6 @@ class jsonLocal extends Command
     {
         //dd("hola");
         $fileExist = Storage::disk('prueba-ftp')->exists('catalogo_xml/productos.json');
-        dd($fileExist);
         if ($fileExist) {
             $size = storage_path('catalogo_xml/productos.json');
             //Storage::disk('local')->put('public/products.json',Storage::disk('prueba-ftp')->get('catalogo_xml/productos.json'));
@@ -39,53 +38,53 @@ class jsonLocal extends Command
             dd("No existe");
         }
         $size = filesize(storage_path('catalogo_xml/productos.json'));
-        switch ($size) {
-            case 655360:
-                sleep(600);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 1 Tamaño: " . $size;
-                break;
-            case 1310720:
-                sleep(540);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 2 Tamaño: " . $size;
-                break;
-            case 1966080:
-                sleep(480);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 3 Tamaño: " . $size;
-                break;
-            case 2621440:
-                sleep(420);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 4 Tamaño: " . $size;
-                break;
-            case 3276800:
-                sleep(360);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 5 Tamaño: " . $size;
-                break;
-            case 3363724832160:
-                sleep(300);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 6 Tamaño: " . $size;
-                break;
-            case 3932160:
-                sleep(240);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 7 Tamaño: " . $size;
-                break;
-            case 4587520:
-                sleep(180);
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 8 Tamaño: " . $size;
-                break;
-            default:
-                // $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
-                $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 0 Tamaño: " . $size;
-                break;
-                //$texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 0 Tamaño: " .$size;
-                Storage::append("RegistroJson.txt", $texto);
-        }
+        // switch ($size) {
+        //     case 655360:
+        //         sleep(600);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 1 Tamaño: " . $size;
+        //         break;
+        //     case 1310720:
+        //         sleep(540);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 2 Tamaño: " . $size;
+        //         break;
+        //     case 1966080:
+        //         sleep(480);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 3 Tamaño: " . $size;
+        //         break;
+        //     case 2621440:
+        //         sleep(420);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 4 Tamaño: " . $size;
+        //         break;
+        //     case 3276800:
+        //         sleep(360);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 5 Tamaño: " . $size;
+        //         break;
+        //     case 3363724832160:
+        //         sleep(300);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 6 Tamaño: " . $size;
+        //         break;
+        //     case 3932160:
+        //         sleep(240);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 7 Tamaño: " . $size;
+        //         break;
+        //     case 4587520:
+        //         sleep(180);
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 8 Tamaño: " . $size;
+        //         break;
+        //     default:
+        //         $this->call('\App\Http\Controllers\PreciosController@lecturaPrueba');
+        //         $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 0 Tamaño: " . $size;
+        //         break;
+        // }
+        $texto = "Archivo Cargado [" . date("Y-m-d H:i:s") . "], Error: 0 Tamaño: " .$size;
+        Storage::append("RegistroJson.txt", $texto);
     }
 }
